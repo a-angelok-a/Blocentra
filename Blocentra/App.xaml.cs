d137ff90db3aacd1d2fd8a49c7b5b3e3c4479eba
Astrael <angelunavv@gmail.com>
2025-08-25 11:22:02 +0700

    Add Prism for MVVM. Create MainView. Configure project to use Prism

--- Blocentra/App.xaml.cs
+++ Blocentra/App.xaml.cs
@@ -1,8 +1,17 @@
-﻿using System.Windows;
+﻿using Blocentra.Views;
+using System.Windows;
 
 namespace Blocentra
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+        protected override Window CreateShell()
+        {
+            return ContainerLocator.Container.Resolve<MainView>();
+        }
     }
 }
